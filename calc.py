871a83f022d5281291eaac473e5f5858fba7e274
Gnoale <g.noale@kayrros.com>
2022-04-25 18:33:45 +0200

    part 2/3

--- calc.py
+++ calc.py
@@ -1,8 +1,9 @@
+#!/usr/bin/env python3
 # Token types
 #
 # EOF (end-of-file) token is used to indicate that
 # there is no more input left for lexical analysis
-INTEGER, PLUS, EOF = 'INTEGER', 'PLUS', 'EOF'
+INTEGER, PLUS, MINUS, PRODUCT, EOF = 'INTEGER', 'PLUS', 'MINUS', 'PRODUCT', 'EOF'
 
 
 class Token(object):
@@ -36,9 +37,35 @@ class Interpreter(object):
         self.pos = 0
         # current token instance
         self.current_token = None
+        self.current_char = self.text[self.pos]
+        self.terms = []
 
     def error(self):
         raise Exception('Error parsing input')
+
+    def advance(self):
+        # is self.pos index past the end of the self.text ?
+        # if so, then return EOF token because there is no more
+        # input left to convert into tokens
+        self.pos += 1
+        if self.pos > len(self.text) - 1:
+            self.current_char = None
+            return
+
+        self.current_char = self.text[self.pos]
+
+
+    def get_number(self):
+        num = ""
+        while self.current_char != None and self.current_char.isdigit():
+            num += self.current_char
+            self.advance()
+        return int(num)
+
+    def skip_space(self):
+        while self.current_char != None and self.current_char.isspace():
+            self.advance()
+
 
     def get_next_token(self):
         """Lexical analyzer (also known as scanner or tokenizer)
@@ -48,28 +75,30 @@ class Interpreter(object):
         """
         text = self.text
 
-        # is self.pos index past the end of the self.text ?
-        # if so, then return EOF token because there is no more
-        # input left to convert into tokens
         if self.pos > len(text) - 1:
             return Token(EOF, None)
 
-        # get a character at the position self.pos and decide
-        # what token to create based on the single character
-        current_char = text[self.pos]
+        if self.current_char.isspace():
+            self.skip_space()
+            return self.get_next_token()
 
-        # if the character is a digit then convert it to
-        # integer, create an INTEGER token, increment self.pos
-        # index to point to the next character after the digit,
-        # and return the INTEGER token
-        if current_char.isdigit():
-            token = Token(INTEGER, int(current_char))
-            self.pos += 1
+        if self.current_char.isdigit():
+            token = Token(INTEGER, self.get_number())
             return token
 
-        if current_char == '+':
-            token = Token(PLUS, current_char)
-            self.pos += 1
+        if self.current_char == '+':
+            token = Token(PLUS, self.current_char)
+            self.advance() 
+            return token
+
+        if self.current_char == '-':
+            token = Token(MINUS, self.current_char)
+            self.advance() 
+            return token
+
+        if self.current_char == '*':
+            token = Token(PRODUCT, self.current_char)
+            self.advance() 
             return token
 
         self.error()
@@ -89,27 +118,32 @@ class Interpreter(object):
         # set current token to the first token taken from the input
         self.current_token = self.get_next_token()
 
-        # we expect the current token to be a single-digit integer
         left = self.current_token
         self.eat(INTEGER)
 
-        # we expect the current token to be a '+' token
         op = self.current_token
-        self.eat(PLUS)
+        self.eat(op.type)
 
-        # we expect the current token to be a single-digit integer
         right = self.current_token
         self.eat(INTEGER)
         # after the above call the self.current_token is set to
         # EOF token
 
-        # at this point INTEGER PLUS INTEGER sequence of tokens
-        # has been successfully found and the method can just
-        # return the result of adding two integers, thus
-        # effectively interpreting client input
-        result = left.value + right.value
-        return result
 
+        if op.type == PLUS:
+            result = left.value + right.value
+        if op.type == MINUS:
+            result = left.value - right.value
+        if op.type == PRODUCT:
+            result = left.value * right.value
+
+        return result
+    
+    def parse(self):
+        self.current_token = self.get_next_token()
+        while self.current_token.value != None:
+            self.terms.append(self.current_token.value)
+            self.eat(self.current_token.type)
 
 def main():
     while True:
